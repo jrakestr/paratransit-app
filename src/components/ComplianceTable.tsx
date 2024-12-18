240a1eed67b9872bdfe6c3b321e893bf71f5e88a
jrakestr <jrakestr@gmail.com>
2024-12-17 18:59:43 -0600

    added Pending to Compliance Table

--- src/components/ComplianceTable.tsx
+++ src/components/ComplianceTable.tsx
@@ -7,7 +7,7 @@ interface ComplianceItem {
   items: {
     name: string;
     identifier: string;
-    status: 'Valid' | 'Expiring' | 'Expired';
+    status: 'Valid' | 'Expiring' | 'Expired' | 'Pending';
     expiration: string;
     daysRemaining: number;
   }[];
